d62ec67d7fd742a5bfdcc1feecba8c65e89ad045
Darshan Jadhav <darshanjadhav363@gmail.com>
2024-11-28 10:14:12 +0530

    feat: Program3

---
+++ Program3/index.js
@@ -0,0 +1,23 @@
+// Write a program to calculating the minimum number of notes required for a given withdrawal amount at an ATM?
+
+function MinNotes(amt) {
+    let notes = [500, 200, 100, 50, 20, 10, 5];
+    let noteCount = {};
+
+    for (let note of notes) {
+        if (amt >= note) {
+            noteCount[note] = Math.floor(amt / note);
+            amt = amt % note;
+        }
+    }
+
+
+    for (let note of notes) {
+        if (noteCount[note]) {
+            console.log(`${note}: ${noteCount[note]} notes`);
+        }
+    }
+}
+
+
+MinNotes(850);
